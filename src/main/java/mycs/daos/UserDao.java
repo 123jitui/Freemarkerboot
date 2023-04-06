072a28f05fec4199c1271dd56e02f57fd3fbe215
ruoyan <a>
2023-04-06 11:37:06 +0800

    qql

--- src/main/java/mycs/daos/UserDao.java
+++ src/main/java/mycs/daos/UserDao.java
@@ -35,145 +35,142 @@ public class UserDao {
 
  		List<User> list1 = userMapper.queryUser(pageNo,pageSize);
 
-
-
-
 // 		List<User> list1 = session.createQuery("from User").setFirstResult(pageNo).setMaxResults(pageSize).list();
 		 
 		
 		return list1;
 	}
 
-	public int qushangye(int pageNo,int pageSize){
-
-// 		session = getSession();
-
-		Object count = userMapper.qushangye(pageNo,pageSize);
-// 		Object count = session.createQuery("select count(*) from User").uniqueResult();//31 / 5 = 6.1
-		int a  = Integer.parseInt(count.toString());
-		System.out.println(a);
-		if(a % pageSize == 0){
-			pageNo--;
-
-		}
-
-		return pageNo;
-	}
- 
-	public int sums(int pageSize){
-
-// 		session = getSession();
-
-
-// 		Object count = session.createQuery("select count(*) from User").uniqueResult();
-
-		Object count = userMapper.sums(pageSize);
-
-		int a  = Integer.parseInt(count.toString());
-		int sum = 0;
-		if(a % pageSize == 0){
-			sum = a / pageSize;
-		}else {
-			sum = a / pageSize;
-			sum++;
-		}
-
-		return sum;
-	}
+//	public int qushangye(int pageNo,int pageSize){
 //
+//// 		session = getSession();
 //
-//
-//
-//
-	 public void delete(int id){
-
-
-
-//		  session = getSession();
-
-//		  User user = new User();
-//
-//		  user.setId(id);
-//		  session.delete(user);
-
-		 userMapper.delete(id);
-		 
-	  }
-//
-//		public List<Gs> qugs() {
-//			
-//			session = getSession();
-//
-////			List<Gs> list = userMapper.qugs();
-//
-// 			List<Gs> list = session.createQuery("from Gs").list();
-//
-//			return list;
+//		Object count = userMapper.qushangye(pageNo,pageSize);
+//// 		Object count = session.createQuery("select count(*) from User").uniqueResult();//31 / 5 = 6.1
+//		int a  = Integer.parseInt(count.toString());
+//		System.out.println(a);
+//		if(a % pageSize == 0){
+//			pageNo--;
 //
 //		}
 //
+//		return pageNo;
+//	}
+//
+//	public int sums(int pageSize){
+//
+//// 		session = getSession();
+//
+//
+//// 		Object count = session.createQuery("select count(*) from User").uniqueResult();
+//
+//		Object count = userMapper.sums(pageSize);
+//
+//		int a  = Integer.parseInt(count.toString());
+//		int sum = 0;
+//		if(a % pageSize == 0){
+//			sum = a / pageSize;
+//		}else {
+//			sum = a / pageSize;
+//			sum++;
+//		}
+//
+//		return sum;
+//	}
+////
+////
+////
+////
+////
+//	 public void delete(int id){
+//
+//
+//
+////		  session = getSession();
+//
+////		  User user = new User();
+////
+////		  user.setId(id);
+////		  session.delete(user);
+//
+//		 userMapper.delete(id);
+//
+//	  }
+////
+////		public List<Gs> qugs() {
+////
+////			session = getSession();
+////
+//////			List<Gs> list = userMapper.qugs();
+////
+//// 			List<Gs> list = session.createQuery("from Gs").list();
+////
+////			return list;
+////
+////		}
+////
+////
+////
+////
+////
+//	 public void update(User user) {
 //
 //
 //
 //
-	 public void update(User user) {
-
-
-		 
-
-
-		 userMapper.update(user);
-
-		
-	 }
-//	 
-//	 
-//	 
-// 
 //
-	 public void add(User user) {
-		 
-//		 session = getSession();
+//		 userMapper.update(user);
 //
-//		 session.persist(user);
-
-		 userMapper.add(user);
-	      
-	 }
-//	 
-//	 
-	 public User queryid(int id) {
-
-
-//		 session = getSession();
 //
-//		 User user = session.get(User.class, id);
-
-		 User user = userMapper.queryid(id);
-		 
-		 
-		 return user;
-
-
-	 }
-
-	public void addlist(List<User> user) {
-
-		for (User user1 : user) {
-			userMapper.add(user1);
-		}
-
-	}
-
-	public List<User> querysy() {
-
-		List<User> user= userMapper.querysy();
-
-//		System.out.println(user.toString());
-		return user;
-
-
-	}
+//	 }
+////
+////
+////
+////
+////
+//	 public void add(User user) {
+//
+////		 session = getSession();
+////
+////		 session.persist(user);
+//
+//		 userMapper.add(user);
+//
+//	 }
+////
+////
+//	 public User queryid(int id) {
+//
+//
+////		 session = getSession();
+////
+////		 User user = session.get(User.class, id);
+//
+//		 User user = userMapper.queryid(id);
+//
+//
+//		 return user;
+//
+//
+//	 }
+//
+//	public void addlist(List<User> user) {
+//
+//		for (User user1 : user) {
+//			userMapper.add(user1);
+//		}
+//
+//	}
+//
+//	public List<User> querysy() {
+//
+//		List<User> user= userMapper.querysy();
+//
+////		System.out.println(user.toString());
+//		return user;
+//
+//
+//	}
 
 
 
--- src/main/java/mycs/daos/UserMapper.java
+++ src/main/java/mycs/daos/UserMapper.java
@@ -3,28 +3,29 @@ package mycs.daos;
 
 import mycs.en.User;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface UserMapper {
 
 
-	
+	@Select("select * from users limit #{pageNo},#{pageSize}")
 	public List<User> queryUser(@Param("pageNo") int pageNo, @Param("pageSize")int pageSize);
 	
-	public int sums(int pageSize);
-	
-	public int qushangye(@Param("pageNo")int pageNo,@Param("pageSize")int pageSize);
-	
-	public void delete(int id);
-	
-	public void add(User user);
-	
-	 public User queryid(int id);
-	 
-	 public void update(User user);
-
-	 public List<User> querysy();
+//	public int sums(int pageSize);
+//
+//	public int qushangye(@Param("pageNo")int pageNo,@Param("pageSize")int pageSize);
+//
+//	public void delete(int id);
+//
+//	public void add(User user);
+//
+//	 public User queryid(int id);
+//
+//	 public void update(User user);
+//
+//	 public List<User> querysy();
 
 
 
